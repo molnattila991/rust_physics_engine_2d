65ec7a319e519aa2eac4885587b72e3be6938430
molnattila991 <molnarattila991@gmail.com>
2022-03-20 15:29:00 +0100

    (edit) add controll logic

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Point;
 
-use physics_engine_2d::graphics::draw::{Draw, DrawCircle};
+use physics_engine_2d::graphics::draw::{DrawCircle};
 
 pub fn main () {
     let sdl_context = sdl2::init().unwrap();
@@ -26,13 +26,17 @@ pub fn main () {
     canvas.clear();
     canvas.present();
 
-    
+    let mut position = Point::new(100,100);
+    let mut velocity = Point::new(0,0);
+
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
         canvas.set_draw_color(LIGHT_BLUE);
         canvas.clear();
 
         for event in event_pump.poll_iter() {
+            set_velocity(&event, &mut velocity);
+
             match event {
                 Event::Quit { .. } |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -42,13 +46,33 @@ pub fn main () {
             }
         }
         
+        position = Point::new(position.x + velocity.x, position.y + velocity.y);
+
         // canvas.set_draw_color(LIGHT_BLUE);
         // canvas.clear();
         let result = canvas.draw_circle_with_color(Point::new(100,100), 510.0, WHITE);
         let result = canvas.draw_circle_with_color_thick(Point::new(11,11), 10.0, 3, RED);
-        let result = canvas.draw_circle_with_color_filled(Point::new(44,44), 20.0, LIGHT_GREEN);
+        let result = canvas.draw_circle_with_color_filled(position, 20.0, LIGHT_GREEN);
 
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 120));
+    }
+}
+
+fn set_velocity(event: &Event, velocity: &mut Point) {
+    match event {
+        Event::KeyDown {  repeat: false, keycode: Some(Keycode::Down), .. } => {
+            velocity.y = velocity.y + 1;
+        },
+        Event::KeyDown { repeat: false, keycode: Some(Keycode::Up), .. } => {
+            velocity.y = velocity.y - 1;
+        },
+        Event::KeyDown { repeat: false, keycode: Some(Keycode::Left), .. } => {
+            velocity.x = velocity.x - 1;
+        },
+        Event::KeyDown { repeat: false, keycode: Some(Keycode::Right), .. } => {
+            velocity.x = velocity.x + 1;
+        }
+        _ => {}
     }
 }
